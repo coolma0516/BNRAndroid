7d08437d0a23a042307cd78c178ba639c144a85c
Jian Ma <rosegun38@163.com>
2018-04-03 23:09:34 +0800

    change toast gravity

--- GeoQuiz/app/src/main/java/com/mageshuo/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/mageshuo/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.mageshuo.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,9 +21,11 @@ public class QuizActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 100);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
@@ -30,9 +33,12 @@ public class QuizActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 100);
+                toast.show();
             }
         });
     }
